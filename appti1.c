1b9966da3a93d091a758333f1f630ce876a92aa6
priyankarenake797 <119857003+priyankarenake797@users.noreply.github.com>
2023-01-30 10:17:23 +0530

    Add files via upload

---
+++ appti1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int  Revers(int iNo)
+{
+   int iDigit = 0, iRev = 0,iSum =0;
+   
+    if(iNo == 0)
+    {
+       iNo = -iNo;
+    }
+   while(iNo != 0)
+    {
+       iDigit = iNo % 10;
+       iRev = (iRev * 10)+ iDigit;
+       iNo = iNo / 10;
+     
+     }
+      return iRev;
+}
+
+int main()
+{
+   int iValue =0 , iRet = 0;
+   
+   printf("Enter no : \n");
+   scanf("%d",&iValue);
+  
+   iRet = Revers(iValue);
+   
+   printf("Addition revers no are : %d",iRet);
+
+   return 0;
+}
+
